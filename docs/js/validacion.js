f951ac5b957012f6939cf94dbc95712421beb0b9
sosanormal <sosanormal@hotmail.com>
2022-10-20 15:07:27 -0300

    upd8

---
+++ docs/js/validacion.js
@@ -0,0 +1,21 @@
+/* let form = document.getElementById("formulario");
+localStorage.setItem("submit", false);
+
+function () {
+   'use strict' 
+}
+
+var forms = document.querySelectorAll('.needs-validation')
+
+Array.prototype.slice.call(forms)
+.forEach(function(form) {
+form.addEventListener('submit', function(event){
+    if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+    }
+
+    form.classList.add('was-validates')
+}, false)
+});
+*/
