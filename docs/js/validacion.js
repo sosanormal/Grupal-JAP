7bac9b85a5dbc7cc5ce786cf9ea41894d63f26a9
sosanormal <sosanormal@hotmail.com>
2022-10-27 15:33:42 -0300

    funcionando

--- docs/js/validacion.js
+++ docs/js/validacion.js
@@ -19,3 +19,52 @@ Array.prototype.slice.call(forms)
 }, false)
 });
 })()
+
+function validacion(){
+
+    let password1 = document.getElementById("password1").value;
+    let password2 = document.getElementById("password2").value;
+    let terms = document.getElementById("checkbox").checked;
+
+
+    if (terms==false){
+        document.getElementById("termsNotChecked").innerHTML = "Debe aceptar los términos del servicio";
+        document.getElementById("termsNotChecked2").innerHTML = "Debe aceptar los términos del servicio";
+    }
+
+        if(formulario.checkValidity() && password2===password1 && terms==true){
+            document.getElementById("formulario").reset();
+            terms.checked =false;
+            
+            document.getElementById("invalidPassword").innerHTML=""
+            console.log("Formulario enviado");  
+
+        } else if(!password2===password1){
+            
+            console.log("contraseñas distintas");   
+        }
+    
+}
+
+function checkCheckbox(){
+    let terms = document.getElementById("checkbox").checked;
+    if (terms==true){
+        document.getElementById("termsNotChecked").innerHTML="";
+        document.getElementById("termsNotChecked2").innerHTML="";
+    }
+}
+function onChange() {
+    
+    const password = document.getElementById('password1');
+    const confirm = document.getElementById('password2');
+
+    if (confirm.value === password.value) {
+      confirm.setCustomValidity('');
+      document.getElementById("invalidPassword").innerHTML = ""
+      console.log("si coinciden")
+    } else {
+      confirm.setCustomValidity('Las contraseñas no coinciden');
+      document.getElementById("invalidPassword").innerHTML = "Las contraseñas deben ser iguales"
+      console.log("no coinciden")
+    }
+  }
